08f8a9819807718bda259621988430c7fafd69d4
Taci Shlosberg <tshlosberg@gmail.com>
2020-06-11 20:25:02 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -12,7 +12,9 @@ meal_choice("carrots", "string beans")
 
 
 def meal_choice(veg1, veg2, protein = "tofu")
- 
+  
+    puts "What a nutritious meal!"
+
     puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
